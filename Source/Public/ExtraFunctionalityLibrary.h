7c2d1edb12dfe98f587028b9a2a5de0519ef9a03
Nicholas Helish <nhelish23@gmail.com>
2019-09-29 16:33:02 -0500

    Forgot to make those functions static

--- Source/Public/ExtraFunctionalityLibrary.h
+++ Source/Public/ExtraFunctionalityLibrary.h
@@ -273,11 +273,11 @@ public:
 
 		/** Gets the closest component to the inputted point(in world space) from the inputted array of components */
 		UFUNCTION(BlueprintPure, Category = "Extra Functionality Library")
-			UPrimitiveComponent* GetClosestComponentToPoint(TArray<UPrimitiveComponent*> Comps, FVector Point);
+			static UPrimitiveComponent* GetClosestComponentToPoint(TArray<UPrimitiveComponent*> Comps, FVector Point);
 
 		/** Gets the closest actor to the inputted point(in world space) from the inputted array of actors */
 		UFUNCTION(BlueprintPure, Category = "Extra Functionality Library")
-			AActor* GetClosestActorToPoint(TArray<AActor*> Actors, FVector Point);
+			static AActor* GetClosestActorToPoint(TArray<AActor*> Actors, FVector Point);
 
 		/**
 		* Draws coordinate arrows at InComponent's world location for DebugDisplayTime's seconds if InComponent is valid.
